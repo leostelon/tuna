81c4d446bdfcb75754dc2b18f32da0595f60738c
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-10-21 17:17:15 +0530

    bridge folder

---
+++ lib/screens/bridge/select.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BridgeSelect extends StatelessWidget {
+  const BridgeSelect({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bridge"),
+      ),
+    );
+  }
+}
